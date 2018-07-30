559e8d340ede204a04d3a3f7410fca5a35eaac39
Cynthia Zheng <xzcynthiazheng@gmail.com>
2018-07-30 19:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
   }
-  if (string.)
+  if (string.isUpperCase()) {
+    return 
+  }
 }
 
