2e1d73d0045677e383e21fa45f847125393ec08d
Cynthia Zheng <xzcynthiazheng@gmail.com>
2018-07-30 19:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function shout(string) {
 }
 
 function whisper(string) {
-    return string.toLowerCase
+    return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 
 }
 
