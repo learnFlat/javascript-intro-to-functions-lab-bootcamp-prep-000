c208935bb03e70a0c15da4272a1396767f0129a9
Cynthia Zheng <xzcynthiazheng@gmail.com>
2018-07-30 19:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function logShout(string) {
   console.log(string.toLowerCase)
 }
 
+function logWhisper(string) {
+  console.log(string.toUpperCase)
+  console.log(string.toLowerCase)
+}
+
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
